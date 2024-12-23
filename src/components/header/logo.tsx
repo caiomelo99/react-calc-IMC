9ed0d5c70271a11b7682714d248f9a9fcc68784b
caiomelo99 <melocaio08@gmail.com>
2024-12-23 10:57:39 -0300

    mudança logo

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -1,7 +1,8 @@
 export const Logo = () => {
     return (
-        <div className='bg-blue-600 w-20 flex justify-center items-center py-2 cursor-pointer'>
-            <p className='text-white text-sm'>Calc <strong>IMC</strong></p>
+        <div className='w-[250px] flex items-center cursor-pointer'>
+            <p className='text-white bg-[#227C9D] px-2 py-1'><strong>IMC</strong></p>
+            <p className="text-gray-500 text-sm ml-[5px]">powered by Caio Melo</p>
         </div>
         //<img src={poweredImage.src} alt="logo" className='w-[150px]'/>
     )
